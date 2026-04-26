95f5ad3dfbd4bed095bc138afdb52613946d73f3
dianbanjiu <dianbanjiu@gmail.com>
2026-04-27 00:49:08 +0800

    refactor(plugins): 将 Git 相关插件配置合并到单一文件

---
+++ lua/plugins/specs/git.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("gitsigns").setup({
+        current_line_blame = true,
+      })
+    end,
+  },
+
+  {
+    "sindrets/diffview.nvim",
+    dependencies = "nvim-lua/plenary.nvim"
+  },
+
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = { "LazyGit", "LazyGitCurrentFile" },
+  }
+}
